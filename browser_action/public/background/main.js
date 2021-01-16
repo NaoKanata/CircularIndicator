3d6fa3c4546a7521fbdd3741bf8052d09e8df8b7
NaoKanata <kanatanao7o@gmail.com>
2021-01-17 01:35:03 +0900

    Create Prototype

--- browser_action/public/background/main.js
+++ browser_action/public/background/main.js
@@ -1,1 +1,26 @@
-alert("background.js");
+localStorage.setItem("progress", 0);
+localStorage.setItem("length", 0);
+setTimeout(timer, 100);
+
+var data = 0
+function timer() {
+    var length = localStorage.getItem("length");
+    var nowDate = localStorage.getItem("nowDate");
+    if (length != 0)
+    {
+        var interval = ((new Date()).getTime() - nowDate) / (1000);
+        var progress = Math.floor(interval / length * 100);
+        if (progress >= 100)
+        {
+            localStorage.setItem("progress", 100);
+            localStorage.setItem("length", 0);
+            alert("終了しました！");
+        }
+        else
+        {
+            localStorage.setItem("progress", progress);
+        }
+        data+=1;
+    }
+    setTimeout(timer, 100);
+}
--- browser_action/src/App.js
+++ browser_action/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Circle from 'react-circle'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            progress: 0,
+            hour: 0,
+            minute: 0,
+            second: 0,
+        }
+        setTimeout(this._timer, 100);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Circle
+                    animate={false}
+                    size={80}
+                    lineWidth={40}
+                    progress={this.state.progress}
+                    bgColor="rgb(150,10,0)"
+                    progressColor="rgb(255,120,0)"
+                    showPercentage={false}
+                    roundedStroke={true}
+                />
+                <input name="hour" type="number" onChange={this._inputChange} />
+                <input name="minute" type="number" onChange={this._inputChange} />
+                <input name="second" type="number" onChange={this._inputChange} />
+                <a onClick={this._click}>PRESS</a>
+                <p>length : {localStorage.getItem("length")}</p>
+                <p>{localStorage.getItem("progress")}</p>
+            </div>
+        );
+    }
+
+    _inputChange = (event) => {
+        const target = event.target;
+        this.setState({
+            [target.name] : target.value
+        });
+    }
+
+    _timer = () => {
+        // background.js で算出された localStorage.getItem("progress") を取得するようにする
+        this.setState({
+            progress: localStorage.getItem("progress")
+        });
+        setTimeout(this._timer, 100)
+    }
+
+    _click = () => {
+        // ここでは必要な情報を送る
+        // - インターバルの長さ
+        // - 開始時刻
+        localStorage.setItem("length", this.state.hour*60*60+this.state.minute*60+this.state.second);
+        localStorage.setItem("nowDate", (new Date()).getTime());
+    }
 }
 
 export default App;
